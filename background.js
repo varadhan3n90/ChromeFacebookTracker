0b859fea993b03e53ed0c523bd96e3eab0ba8a43
Varadhan <varadhan3n90@yahoo.co.in>
2012-10-21 21:26:45 +0530

    First commit. Created alert after 1 hr. Polls active tabs every 3sec.

---
+++ background.js
@@ -0,0 +1,102 @@
+function tabUpdated(tabId, changeInfo, tab)
+{
+	checkSocial(tab);
+}
+
+function checkSocial(tab)
+{
+	var tablink = tab.url;
+	var site = /google/i;
+	var fb = "google"
+	if ( tablink.match(site) == fb )
+		checkCookie();
+	
+}
+
+// Trying out cookies
+function getCookie(c_name)
+{
+var i,x,y,ARRcookies=document.cookie.split(";");
+for (i=0;i<ARRcookies.length;i++)
+  {
+  x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+  y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+  x=x.replace(/^\s+|\s+$/g,"");
+  if (x==c_name)
+    {
+    return unescape(y);
+    }
+  }
+}
+
+function setCookie(c_name,value,exdays)
+{
+	var exdate=new Date();
+	exdate.setDate(exdate.getDate() + exdays);
+	var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+	document.cookie=c_name + "=" + c_value;
+}
+
+function checkCookie()
+{
+	var days= getCookie("days");
+	var hrs = getCookie("hrs");
+	var min = getCookie("min");
+	var sec = getCookie("sec");
+	var dt1 = new Date();
+	var dt2 = getCookie("today");
+	var alrm = getCookie("alarmFired");
+	if ( days == null  || days == "")
+	{
+		var dt = new Date();
+		setCookie("days","0",365);
+		setCookie("hrs","0",365);
+		setCookie("min","0",365);
+		setCookie("sec","0",365);
+		setCookie("startdate",dt,365);
+	}
+	else
+	{		
+		sec = parseInt(sec) + 3;
+		if(alrm=="false"&&Math.abs(new Date(dt2)-dt1)>(1*60*60*1000))
+		{
+			alert("You have been using facebook for more than an hour today.");
+			setCookie("alarmFired","true",1);
+		}
+		if ( sec >= 60 )
+		{			
+			min = parseInt(min) + 1 ;
+			sec = sec - 60;
+			if ( min >= 60 )
+			{			
+				hrs = parseInt(hrs) + 1 ;
+				min = min - 60;
+				if ( hrs >= 24 )
+				{
+					days = parseInt(days) + 1;
+					hrs = hrs - 24;
+				}
+			}
+		}
+		setCookie("sec",sec,10);
+		setCookie("days",days,365);
+		setCookie("hrs",hrs,365);
+		setCookie("min",min,365);
+	}
+	
+}
+
+setInterval(function(){chrome.tabs.getSelected(null,checkSocial)},3000);
+chrome.tabs.onUpdated.addListener(tabUpdated);
+var date = new Date();
+var date1 = getCookie("today");
+if (date1 !=null )
+	date1 = new Date(date1);
+if (date1 !=null && Math.abs(date-date1)<1*60*60)
+{
+	
+}else
+{
+	setCookie("today",date,10);
+	setCookie("alarmFired","false",10);
+}
---
+++ popup.js
@@ -0,0 +1,69 @@
+// Trying out cookies
+function getCookie(c_name)
+{
+var i,x,y,ARRcookies=document.cookie.split(";");
+for (i=0;i<ARRcookies.length;i++)
+  {
+  x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+  y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+  x=x.replace(/^\s+|\s+$/g,"");
+  if (x==c_name)
+    {
+    return unescape(y);
+    }
+  }
+}
+
+function setCookie(c_name,value,exdays)
+{
+	var exdate=new Date();
+	exdate.setDate(exdate.getDate() + exdays);
+	var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+	document.cookie=c_name + "=" + c_value;
+}
+
+function checkCookie()
+{
+	var days= getCookie("days");
+	var hrs = getCookie("hrs");
+	var min = getCookie("min");
+	var sec = getCookie("sec");
+	var dt = getCookie("startdate");
+	var ptag = document.getElementById("ptag");
+	var msg = "You have been using facebook for ";
+	if (days != null || days != "") {
+		dt = new Date(dt);
+		var date = dt.getDate();
+		var month = dt.getMonth() + 1;
+		var year = dt.getFullYear();
+		if (days!=0)
+			msg = msg + days +" days "+ hrs +" hours " ;//+ min + " min "+ sec + " sec "  ;
+		else if (hrs!=0)
+			msg = msg + hrs +" hours " + min + " min ";//+ sec + " sec "  ;
+		else if (min!=0)
+			msg = msg + min + " min "+ sec + " sec "  ;
+		else
+			msg = msg + sec + " seconds ";
+	msg = msg + " starting " + date +"-"+ month+"-"+year;
+		ptag.innerHTML = msg;
+	}
+	else
+		ptag.innerHTML = "Timer not yet started";
+}
+
+function resetTimer()
+{
+	var dt = new Date();
+	setCookie("days","0",365);
+	setCookie("hrs","0",365);
+	setCookie("min","0",365);
+	setCookie("sec","0",365);
+	setCookie("startdate",dt,365);
+}
+function createButton()
+{
+	var button = document.getElementById("resetButton");
+	button.onclick = resetTimer;	
+}
+checkCookie();
+createButton();
